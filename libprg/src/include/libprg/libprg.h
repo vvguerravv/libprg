882589b361893ab5e09682e814423bfb470e50ab
Victor Guerra <victor.eg@aluno.ifsc.edu.br>
2023-09-05 09:21:47 -0300

    Usando biblioteca

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,6 +1,8 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
+
+
 typedef struct {
     double value;
     int error;
@@ -8,8 +10,23 @@ typedef struct {
 
 typedef enum { SUM, SUB } operation_t;
 
+
+
 result_t sub(double a, double b);
 result_t sum(double a, double b);
 result_t compute(double a, double b, operation_t op);
+
+/**
+ *
+ * @param vetor
+ * @param total
+ * @param elemento
+ * @return 0 se ok e 1 se não ok
+ */
+int insere_num(int *vetor,int *total,int tam_vetor,int elemento);
+int remove_num(int *vetor,int *total,int elemento);
+int busca_linear(int *vetor,int elemento);
+int busca_bin_int(int *vetor,int elemento);
+int busca_bin_rec(int *vetor,int elemento);
 
 #endif
---
+++ libprg/src/libprg/lista_linear_naoordenada.c
@@ -0,0 +1,7 @@
+#include <libprg/libprg.h>
+
+int insere_num(int *vetor,int *total,int tam_vetor,int elemento)
+{
+
+    return 0;
+}
