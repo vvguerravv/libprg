a0125a662caa11cce93c21c7f5c739897c79d061
Victor Guerra <victor.eg@aluno.ifsc.edu.br>
2023-09-11 19:33:10 -0300

    Biblioteca

--- libprg/src/libprg/lista.c
+++ libprg/src/libprg/lista.c
@@ -18,3 +18,18 @@ int insere_nao_ord(int *vetor,int *total,int tamanho,int elemento)
 
     return 0;
 }
+
+int insere_ord(int *vetor,int *total,int tamanho,int elemento)
+{
+    if(tamanho >= total){
+        for(int i = 0; i < tamanho; i++){
+            if(elemento < vetor[i]){
+                for(int j = total; vetor[i] != vetor[j]; j--);{
+                }
+            }
+        }
+    }
+
+
+    return 0;
+}
